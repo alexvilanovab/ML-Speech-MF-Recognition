a932fe7efc530e34e351dd9f696bfd0c8f95c05f
Xavi Gordillo <xavigordillo@client170-10-192-213.eduroam.upc.edu>
2019-02-06 17:49:36 +0100

    Removed audio analysis from model.py

--- model.py
+++ model.py
@@ -27,34 +27,10 @@ serial_rfc = pickle.dump(rfc, f)
 
 scores = cross_val_score(rfc, X, y, cv = 50)
 print("Model trained.")
+print("Model saved as trained_classifier")
 #Calculates the accuracy score
 print('Acuracy: ', round(scores.mean()*100,2), '%')
 
 #Calculates the training time
 end = time.time()
 print("Training time: ", round(end-start,2), "s")
-
-print("Measuring acoustic parameters...")
-start = time.time()
-os.system("Rscript wav_analyzer.r >/dev/null 2>&1")
-print("Done.")
-end = time.time()
-print("Analysis time: ", round(end-start,2), "s")
-
-df_pred = pd.read_csv('testData.csv')
-#Reads audio data from testData
-df_names = df_pred['sound.files'].copy()
-#Saves names column to display them later
-df_pred.drop(['sound.files', 'duration', 'selec', 'peakf', 'Unnamed: 0'], axis=1, inplace=True) #inplace=True
-#Leaves only the columns that matter to make the prediction
-prediction = rfc.predict(df_pred)
-#Creates an array of ints that represent the predicted gender of each row of the dataFrame
-
-print("-"*10)
-for i in range(len(prediction)):
-    #Displays the predicted genders in a formatted way
-    print(df_names[i].split('.')[0].title(), "is a ", end='')
-    if prediction[i] == 0:
-        print("male")
-    else:
-        print("female")
